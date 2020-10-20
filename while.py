1550d918715e1fc3dc7958e09e1e7b3c96f02a1b
Yulia Shumeiko <shumeyko007@ukr.net>
2020-10-20 11:41:49 +0300

    1

--- while.py
+++ while.py
@@ -1,1 +1,12 @@
-print(5)
+# Пока While не разлучит нас
+def add(a, b, c):
+    while a <= b:
+        print("Значение a {0} пока что нет".format(a))
+        a = a + c
+    print("Дождались! Финальный a : {0}".format(a))
+
+a=float(input("Введите число a: "))
+b=float(input("Введите число b: "))
+c=float(input("Введите число c: "))
+
+add(a, b, c)
